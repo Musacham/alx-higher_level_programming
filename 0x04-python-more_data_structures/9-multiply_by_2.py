0ce9314f0f449c025e8ae54edfd8ac82e1c0d194
Musacham <musacham200@gmail.com>
2022-12-08 02:29:39 -0800

    data strc

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(dictionary):
-    new_dict = a_dictionary.copy()
-    for i, j in new_dict.items():
-        new_dict[i] = j*2
-    return new_dict
+    new_dictionary = a_dictionary.copy()
+    for key, value in list(new_dictionary.items()):
+        new_dictionary[key] = value * 2
+    return new_dictionary
+
