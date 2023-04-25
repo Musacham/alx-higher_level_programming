2489dbaea16378878d2384768b9ea08a11bfb41d
Musacham <musacham200@gmail.com>
2023-04-25 03:46:30 -0700

    web_scrapping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@
 const file = process.argv[2];
 const fs = require('fs');
 
-fs.readFil0-readme.jse(file, 'utf8', (error, data) => {
+fs.readFile(file, 'utf8', (error, data) => {
   if (error) {
     console.log(error);
   } else {
